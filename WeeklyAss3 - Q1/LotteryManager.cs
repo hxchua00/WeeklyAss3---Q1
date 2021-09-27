d404f80b005c91d0d1d4942938888828df3d9a6d
hxchua00 <hxchua00@gmail.com>
2021-09-27 16:06:24 +0800

    Add project files.

---
+++ WeeklyAss3 - Q1/LotteryManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyAss3___Q1
+{
+    delegate void LotteryEventHandler(object sender, EventArgs e);
+    class LotteryManager
+    {
+        public event LotteryEventHandler LotteryEvent;
+
+        Random rand = new Random();
+        int[] LotteryNum = new int[12];
+        List<int[]> LotteryTickets = new List<int[]>();
+
+        public void BuyLottery()
+        {
+            for(int i = 0; i < LotteryNum.Length; i++)
+            {
+                LotteryNum[i] = rand.Next(0, 9);
+            }
+
+            LotteryTickets.Add(LotteryNum);
+
+            Console.WriteLine("Ticket bought!");
+            Console.WriteLine("Your number is: ");
+            for(int i = 0; i< LotteryNum.Length; i++)
+            {
+                Console.Write(LotteryNum[i]);
+            }
+            Console.WriteLine();
+           
+        }
+
+        public void SelectWinner()
+        {
+            int wincondition = 0;
+            int[] WinningTicket = new int[12];
+            for (int i = 0; i < WinningTicket.Length; i++)
+            {
+                WinningTicket[i] = rand.Next(0, 9);
+            }
+
+            int[] checkTicket = new int[12];
+           
+          
+                for (int i = 0; i < LotteryTickets.Count; i++)
+                {
+                    checkTicket = LotteryTickets[i];
+                }
+
+                for(int i =0; i< WinningTicket.Length; i++)
+                {
+                    if(WinningTicket[i] == checkTicket[i])
+                    {
+                        wincondition++;
+                    }else if (wincondition == 12)
+                    {
+                        Console.WriteLine("CONGRATULATIONS! WE HAVE A WINNER!");
+                        Console.WriteLine();
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry! The numbers do not match!");
+                        Console.WriteLine("You lose!");
+                        Console.WriteLine();
+                        break;
+                    }
+                }
+            
+            
+            LotteryEventSub(EventArgs.Empty);
+        }
+
+        protected virtual void LotteryEventSub(EventArgs e)
+        {
+            LotteryEvent?.Invoke(this, e);
+        }
+    }
+}
---
+++ WeeklyAss3 - Q1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyAss3___Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            bool loop = true;
+           
+            LotteryManager LM = new LotteryManager();
+            LM.LotteryEvent += EventMessage;
+
+            while (loop)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Do you want to buy a ticket?");
+                Console.WriteLine("1) Yes");
+                Console.WriteLine("2) No");
+                Console.WriteLine("3) Exit");
+                Console.WriteLine();
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        LM.BuyLottery();
+                        count++;
+                        break;
+                    case 2:
+                        if (count >= 10)
+                        {
+                            Console.WriteLine("We have enough tickets to decide for the winner!");
+                            Console.WriteLine();
+                            LM.SelectWinner();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry! Not enough tickets to decide for the winner!");
+                            Console.WriteLine();
+                        }
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("Goodbye!");
+                        loop = false;
+                        break;
+                    default:
+                        Console.WriteLine("Option not available!");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+            
+            
+            Console.ReadLine();
+        }
+
+        public static void EventMessage(object sender, EventArgs e)
+        {
+            Console.WriteLine("Thank you for participating in this lottery!");
+            Console.WriteLine("Good luck next time too!");
+            Console.WriteLine();
+        }
+    }
+}
